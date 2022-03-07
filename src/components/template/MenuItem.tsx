5570bfcd4ea5910d7bce3e03451e0054fa6b8b38
Samm014 <samuel.medeiros014@gmail.com>
2022-03-07 10:29:37 -0300

    add dark mode style

--- src/components/template/MenuItem.tsx
+++ src/components/template/MenuItem.tsx
@@ -1,27 +1,42 @@
 import Link from "next/link"
 
 interface MenuItemProps {
-    url: string
+    url?: string
     text: string
     icon: any
+    className?: string
+    onClick?: (event: any) => void
 }
 
 export default function MenuItem(props: MenuItemProps){
-    return (
-        <li className={`hover:bg-gray-100 cursor-pointer`}>
-            <Link href={props.url}>
-                <a className={`
+
+    function renderLink(){
+        return(
+            <a className={`
                     flex flex-col justify-center items-center
                     h-20 w-20
+                    dark:text-gray-200 
+                    ${props.className}
                 `}>
                     {props.icon}
-                    <span className={`
-                        text-xs font-light text-gray-600
-                    `}>
+                    <span className={`text-xs font-light`}>
                         {props.text}
                     </span>
-                </a>
-            </Link>
+            </a>
+        )
+    }
+    return (
+        <li onClick={props.onClick} className={`
+            hover:bg-gray-100 dark:hover:bg-gray-700 cursor-pointer
+            `}>
+            {props.url ? (
+                <Link href={props.url}> 
+                    {renderLink()}
+                </Link>
+            ):(
+               renderLink()
+            )}
+            
         </li>
     )
 }
--- src/components/template/SideMenu.tsx
+++ src/components/template/SideMenu.tsx
@@ -1,17 +1,34 @@
 import MenuItem from "./MenuItem";
 import Logo from "./Logo";
-import { HomeIcon, SettingsIcon, BellIcon } from "../icons";
+import { HomeIcon, SettingsIcon, BellIcon, ExitIcon } from "../icons";
 
 export default function SideMenu(){
     return (
-        <aside>
+        <aside className={`
+            flex flex-col
+            dark:bg-gray-900
+            bg-gray-200 text-gray-700
+        `}>
             
             <Logo />
 
-            <ul>
+            <ul className="flex-grow">
                 <MenuItem url="/" text="Início" icon={HomeIcon} />
                 <MenuItem url="/ajustes" text="Ajustes" icon={SettingsIcon} />
                 <MenuItem url="/notificacoes" text="Notificações" icon={BellIcon} />
+            </ul>
+            <ul>
+                <MenuItem 
+                    url="/"
+                    text="Sair"
+                    icon={ExitIcon}
+                    onClick={()=> console.log('logout')}
+                    className={`
+                        text-red-600 dark:text-red-400
+                        hover:bg-red-400 hover:text-white
+                        dark:hover:text-white
+                    `}
+                />
             </ul>
         </aside>
     )
